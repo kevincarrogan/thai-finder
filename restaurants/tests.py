ed18cee6038fa845cfb3031a29b6d4b7cb7c04be
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-01-31 17:27:33 +0000

    Add endpoint for random restaurant response.
    Currently only stubbed out to provide a valid response without any actual content.

--- restaurants/tests.py
+++ restaurants/tests.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class RestaurantTestCase(TestCase):
+
+    def test_random_restaurant_endpoint_returns_ok_status_code(self):
+        url = reverse('restaurants:random')
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
 
-# Create your views here.
+
+class RandomRestaurantView(View):
+    
+    def get(self, *args, **kwargs):
+        return HttpResponse('OK')
