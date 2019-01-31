e9e7259a21c5711a00d194e2a570aa1fd8ec046a
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-01-31 23:27:53 +0000

    Refactor top 10 restaurants view.
    Splits the get method out into separate methods and makes adding new filter keys easier.

--- restaurants/views.py
+++ restaurants/views.py
@@ -23,32 +23,54 @@ class RandomRestaurantView(View):
 
 class Top10RestaurantView(View):
 
-    def get(self, request):
+    NUMBER_OF_RESULTS = 10
+
+    def _get_query_set(self):
+        return Restaurant.objects.all()
+
+    def _filter_query_set_from_request(self, query_set, request):
+        foreign_key_filter_keys = [
+            ('cuisine', Cuisine),
+        ]
+        for key, related_model_class in foreign_key_filter_keys:
+            query_value = request.GET.get(key)
+            if query_value:
+                try:
+                    object_to_filter_by = related_model_class.objects.get(name=query_value)
+                except related_model_class.DoesNotExist:
+                    # If we can't find a lookup for our object we can just
+                    # quickly return no results instead of trying more filters
+                    return self._get_query_set().none()
+                else:
+                    query_set = query_set.filter(**{key: object_to_filter_by})
+
+        filter_keys = ['grade']
+        for key in filter_keys:
+            query_value = request.GET.get(key)
+            if query_value:
+                query_set = query_set.filter(**{key: query_value})
+
+        return query_set
+
+    def _limit_query_set(self, query_set):
+        return query_set.order_by('-score')[:self.NUMBER_OF_RESULTS]
+
+    def _get_results(self, query_set):
         results = []
 
-        top_10_restaurants = Restaurant.objects.all()
-
-        cuisine_query = request.GET.get('cuisine')
-        if cuisine_query:
-            try:
-                cuisine_to_filter_by = Cuisine.objects.get(name=cuisine_query)
-            except Cuisine.DoesNotExist:
-                top_10_restaurants = Restaurant.objects.none()
-            else:
-                top_10_restaurants = top_10_restaurants.filter(cuisine=cuisine_to_filter_by)
-
-        grade_query = request.GET.get('grade')
-        if grade_query:
-            top_10_restaurants = top_10_restaurants.filter(grade=grade_query)
-
-        top_10_restaurants = top_10_restaurants.order_by('-score')[:10]
-
-        for restaurant in top_10_restaurants:
+        for restaurant in query_set:
             results.append({
                 'name': restaurant.name,
                 'score': restaurant.score,
             })
 
+        return results
+
+    def get(self, request):
+        query_set = self._get_query_set()
+        filtered_query_set = self._filter_query_set_from_request(query_set, request)
+        limited_query_set = self._limit_query_set(filtered_query_set)
+
         return JsonResponse({
-            'results': results,
+            'results': self._get_results(limited_query_set),
         })
