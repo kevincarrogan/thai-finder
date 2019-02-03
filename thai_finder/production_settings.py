20c940fe538384b9c4b37af0c5c90509039bfcdd
Kevin Carrogan <kevin.carrogan@gmail.com>
2019-02-03 19:32:41 +0000

    Adding settings for production secret key.

--- thai_finder/production_settings.py
+++ thai_finder/production_settings.py
@@ -1,6 +1,14 @@
 import dj_database_url
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+
 
 ALLOWED_HOSTS = ['thai-finder.herokuapp.com']
 DATABASES = {
     'default': dj_database_url.config()
 }
+try:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+except KeyError:
+    raise ImproperlyConfigured('Secret key environment variable required on production.')
